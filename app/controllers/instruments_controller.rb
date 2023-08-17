a45c08a89a64dda82cfe2b019d5fe49bba7c6e6e
David Hickson <davidedwardhickson@gmail.com>
2023-08-17 21:15:03 +0100

    created-instrument-controller

---
+++ app/controllers/instruments_controller.rb
@@ -0,0 +1,38 @@
+class InstrumentsController < ApplicationController
+
+  def index
+    @instruments = Instrument.all
+  end
+
+  # def show
+  #   @instrument = Instrument.find(params[:id])
+  # end
+
+  # def new
+  #   @instrument = Instrument.new
+  # end
+
+  # def create
+  #   instrument = Instrument.new(instrument_params)
+  #   instrument.user = current_user
+  #   if instrument.save
+  #     redirect_to instrument_path(instrument)
+  #   else
+  #     render :new
+  #   end
+  # end
+
+  # private
+  # def instrument_params
+  #   params.require(:instrument).permit(:name, :type, :genre, :description, :price, :location)
+  # end
+
+end
+# db for reference
+
+# #  t.string "name"
+#     t.string "type"
+#     t.string "genre"
+#     t.string "description"
+#     t.integer "price"
+#     t.string "location" %>
