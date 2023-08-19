52f1dd800f28440ad2d898f395953551223c93bc
David Hickson <davidedwardhickson@gmail.com>
2023-08-19 16:24:18 +0100

    created-new-bookingandform

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,9 +3,22 @@ class BookingsController < ApplicationController
     "@bookings = Bookings.all"
   end
 
-  def booking
+  def new
+    @instrument = Instrument.find(params[:instrument_id])
+    @booking = Booking.new
   end
 
-  def create
-  end
+  # def create
+  # booking = Booking.new(booking_params)
+  # booking.user = current_user
+  #   if booking.save
+  #     redirect_to instrument_path(instrument)
+  #   else
+  #     render :new
+  # end
+
+  # private
+  # def booking_params
+  #   params.require(:booking).permit(:name, :type, :genre, :description, :price, :location)
+  # end
 end
